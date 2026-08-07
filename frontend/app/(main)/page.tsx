7c38ce73e820797e1b55f722d0a1171b579b35a2
David Chinonso Nwachukwu <nwadav9@gmail.com>
2026-08-08 00:10:01 +0100

    Make homepage dynamic so product changes reflect immediately

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -1,6 +1,8 @@
 import HomePageOne from "@/components/pages/home-pages/HomePageOne";
 import HomePageTwo from "@/components/pages/home-pages/HomePageTwo";
 
+export const dynamic = "force-dynamic";
+
 export default  function Home() {
 
 
